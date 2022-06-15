cb3a4c871cd2c94caeface7f20c84a83a5dedd4d
iden <dennydharmawan717@gmail.com>
2022-06-15 23:02:24 +0700

    CONFIG -> add secret key in config

--- Config/app.config.js
+++ Config/app.config.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 module.exports = {
+    secret_key: process.env.JWT_SECRETKEY,
     mongodb_url: process.env.MONGODB_URL,
     port: process.env.PORT || 3005,
     client_url: process.env.CLIENT_URL || 'http://localhost:4200',
