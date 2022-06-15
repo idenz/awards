fffe04f8af1843e2a7e7726d178a33ef21cad956
iden <dennydharmawan717@gmail.com>
2022-06-15 16:39:15 +0700

    add dotenv

--- Config/app.config.js
+++ Config/app.config.js
@@ -1,4 +1,9 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
 module.exports = {
+    mongodb_url: process.env.MONGODB_URL,
     port: process.env.PORT || 3005,
     client_url: process.env.CLIENT_URL || 'http://localhost:4200',
 };
